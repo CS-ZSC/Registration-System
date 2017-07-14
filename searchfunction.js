7606d033ccf23a713389d0b0ba0f3ffb685877ef
Salma Magdy <Salma711@users.noreply.github.com>
2017-07-14 03:55:09 +0200

    Search model function

---
+++ searchfunction.js
@@ -0,0 +1,15 @@
+
+<html>
+<body>
+<script type="text/javascript">
+   <!--
+      function SearchFunction()
+      {
+         // search for items in a list 
+		 // the list should include name/age/any required info
+		 /*first we create a search list using HTML then search for items in that list using this function*/ 
+      }
+   //-->
+</script>
+</body>
+</html>
